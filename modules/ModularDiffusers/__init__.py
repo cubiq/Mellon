a29b67302674df22dcf38dfd787bee69645fc690
Alvaro Somoza <somoza.alvaro@gmail.com>
2025-08-26 12:17:38 -0400

    sdxl blocks

--- modules/ModularDiffusers/__init__.py
+++ modules/ModularDiffusers/__init__.py
@@ -4,3 +4,17 @@ from diffusers import ComponentsManager
 components = ComponentsManager()
 
 MODULE_PARSE = ["controlnet", "denoise", "embeddings", "guiders", "latents", "loaders", "schedulers"]
+
+SDXL_BLOCKS = [
+    "down_blocks.1.attentions.0",
+    "down_blocks.1.attentions.1",
+    "down_blocks.2.attentions.0",
+    "down_blocks.2.attentions.1",
+    "mid_block.attentions.0",
+    "up_blocks.0.attentions.0",
+    "up_blocks.0.attentions.1",
+    "up_blocks.0.attentions.2",
+    "up_blocks.1.attentions.0",
+    "up_blocks.1.attentions.1",
+    "up_blocks.1.attentions.2  ",
+]
