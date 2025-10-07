c33124664c9ad2a19ad10f2118de0162db39e971
cubiq <matteo@elf.io>
2025-10-07 20:25:58 +0200

    clean up

--- custom/Example/main.py
+++
@@ -1,8 +0,0 @@
-from mellon.NodeBase import NodeBase
-
-class CustomNode(NodeBase):
-    def execute(self, **kwargs):
-        rgb = kwargs.get("rgb", "0,0,0")
-        rgb = [int(x) for x in rgb.split(",")]
-
-        return { "output": rgb }
--- modules/Experiments/__init__.py
+++ modules/Experiments/__init__.py
@@ -1,70 +1,14 @@
 from utils.torch_utils import DEVICE_LIST, DEFAULT_DEVICE
-from utils.huggingface import get_local_model_ids
-from mellon.modelstore import modelstore
 from .flux_layers import FLUX_LAYERS
+from .t5_layers import T5_LAYERS
+from .sd3_layers import SD3_LAYERS
+from copy import deepcopy
 
 def str_to_none(value, params):
     none_values = ['none', 'null', 'no', 'empty', 'ignore']
     return None if value in none_values else value
 
 MODULE_PARSE = ['StableDiffusion3', 'VAE', 'FLUXKontext', 'StableDiffusionXL']
-
-MODULE_MAP = {
-    "SD3PipelineLoader": {
-        "description": "Load a Stable Diffusion 3 pipeline",
-        "label": "SD3 Pipeline Loader",
-        "category": "loader",
-        "style": { "minWidth": 360 },
-        "params": {
-            "pipeline": { "label": "SD3 Pipeline", "display": "output", "type": "pipeline" },
-            "model_id": {
-                "label": "Model",
-                "display": "autocomplete",
-                "type": "string",
-                'options': modelstore.get_hf_ids(class_name="StableDiffusion3Pipeline"),
-                "default": "stabilityai/stable-diffusion-3.5-large",
-                "fieldOptions": { "noValidation": True }
-            },
-            "dtype": {
-                "label": "Dtype",
-                "type": "string",
-                "default": "bfloat16",
-                "options": ['auto', 'float32', 'float16', 'bfloat16'],
-            },
-            "load_t5": { "label": "Load T5 Encoder", "type": "boolean", "default": True },
-            "text_encoders": { "label": "Text Encoders", "display": "input", "type": "SD3TextEncoders", "onChange": { True: [], False: ['load_t5']} },
-            "transformer": { "label": "Transformer", "display": "input", "type": "SD3Transformer2DModel" },
-        }
-    },
-
-    'SD3TextEncodersLoader': {
-        'label': 'SD3 Text Encoders Loader',
-        'description': 'Load the CLIP and T5 Text Encoders',
-        'category': 'loader',
-        'params': {
-            'encoders': {
-                'label': 'SD3 Encoders',
-                'display': 'output',
-                'type': 'SD3TextEncoders',
-            },
-            'model_id': {
-                'label': 'Model ID',
-                'type': 'string',
-                'default': 'stabilityai/stable-diffusion-3.5-large',
-                'options': modelstore.get_hf_ids(class_name="StableDiffusion3Pipeline"),
-                'display': 'autocomplete',
-                'fieldOptions': { "noValidation": True, "model_loader": True },
-            },
-            'dtype': {
-                'label': 'Dtype',
-                'type': 'string',
-                'default': 'bfloat16',
-                'options': ['auto', 'float32', 'float16', 'bfloat16'],
-            },
-            'load_t5': { 'label': 'Load T5 Encoder', 'type': 'boolean', 'default': True },
-        },
-    },
-}
 
 QUANT_FIELDS = {
     'bnb_group': {
@@ -116,7 +60,6 @@ QUANT_FIELDS = {
             'options': ['none', 'float8', 'int8'],
             'default': 'none',
             'type': 'string',
-            #'postProcess': str_to_none
         },
 
     'torchao_group': {
@@ -148,8 +91,8 @@ QUANT_FIELDS = {
         "description": "Exclude layers from the quantization process.",
         "label": "Exclude Layers",
         "display": "autocomplete",
-        "default": "proj_out",
-        "options": FLUX_LAYERS,
+        "default": "",
+        "options": [],
         "fieldOptions": {
             "multiple": True,
             "disableCloseOnSelect": True
@@ -197,3 +140,208 @@ PREFERRED_KONTEXT_RESOLUTIONS = [
     (1504, 688),
     (1568, 672),
 ]
+
+MODULE_MAP = {
+    'SD3TransformerLoader': {
+        'label': "SD3 Transformer Loader",
+        'category': "loader",
+        'style': { "minWidth": 300 },
+        'resizable': True,
+        'params': {
+            "model_id": {
+            "label": "Model",
+            "display": "modelselect",
+            "type": "string",
+            "default": { 'source': 'hub', 'value': 'stabilityai/stable-diffusion-3.5-large' },
+            "fieldOptions": {
+                "noValidation": True,
+                "sources": ['hub', 'local'],
+                "filter": {
+                    "hub": { "className": ["SD3Transformer2DModel"] },
+                    "local": { "id": r"sd3\.5" },
+                },
+            },
+            },
+            "dtype": {
+                "label": "Dtype",
+                "type": "string",
+                "default": "bfloat16",
+                "options": ['auto', 'float32', 'float16', 'bfloat16'],
+            },
+            **QUANT_SELECT,
+            **deepcopy(QUANT_FIELDS),
+            "fuse_qkv": {
+                "description": "Improve performance at the cost of increased memory usage.",
+                "label": "Fuse QKV projections",
+                "type": "boolean",
+                "default": False,
+            },
+            "compile": {
+                "description": "Use Torch to compile the model for improved performance. Works only on supported platforms.",
+                "label": "Compile",
+                "type": "boolean",
+                "default": False,
+                "onChange": {
+                    True: ['compile_mode', 'compile_fullgraph'],
+                    False: []
+                }
+            },
+            "compile_mode": {
+                "label": "Mode",
+                "type": "string",
+                "default": "max-autotune",
+                "options": ['default', 'reduce-overhead', 'max-autotune', 'max-autotune-no-cudagraphs'],
+            },
+            "compile_fullgraph": {
+                "label": "Full Graph",
+                "type": "boolean",
+                "default": True,
+            },
+            "transformer": { "label": "Transformer", "display": "output", "type": "SD3Transformer2DModel" },
+        }
+    },
+
+    'SD3TextEncodersLoader': {
+        'description': "Load the CLIP and T5 Text Encoders",
+        'label': 'SD3 Text Encoders Loader',
+        'category': 'loader',
+        'resizable': True,
+        'params': {
+            'encoders': {
+                'label': 'SD3 Encoders',
+                'display': 'output',
+                'type': 'SD3TextEncoders',
+            },
+            'model_id': {
+                "label": "Model",
+                "display": "modelselect",
+                "type": "string",
+                "default": { 'source': 'hub', 'value': "stabilityai/stable-diffusion-3.5-large" },
+                "fieldOptions": {
+                    "noValidation": True,
+                    "sources": ['hub', 'local'],
+                    "filter": {
+                        "hub": { "className": ["StableDiffusion3Pipeline"] },
+                        "local": { "id": r"SD3\.5" },
+                    },
+                },
+            },
+            'dtype': {
+                'label': 'Dtype',
+                'type': 'string',
+                'default': 'bfloat16',
+                'options': ['auto', 'float32', 'float16', 'bfloat16'],
+            },
+            **QUANT_SELECT,
+            **deepcopy(QUANT_FIELDS),
+            'load_t5': { 'label': 'Load T5 Encoder', 'type': 'boolean', 'default': True },
+        }
+    },
+
+    'FluxTransformerLoader': {
+        'label': "FLUX Transformer Loader",
+        'category': "loader",
+        'style': { "minWidth": 300 },
+        'resizable': True,
+        'params': {
+            "model_id": {
+            "label": "Model",
+            "display": "modelselect",
+            "type": "string",
+            "default": { 'source': 'hub', 'value': 'black-forest-labs/FLUX.1-dev' },
+            "fieldOptions": {
+                "noValidation": True,
+                "sources": ['hub', 'local'],
+                "filter": {
+                    "hub": { "className": ["FluxTransformer2DModel"] },
+                    "local": { "id": r"flux" },
+                },
+            },
+            },
+            "dtype": {
+                "label": "Dtype",
+                "type": "string",
+                "default": "bfloat16",
+                "options": ['auto', 'float32', 'float16', 'bfloat16'],
+            },
+            **QUANT_SELECT,
+            **deepcopy(QUANT_FIELDS),
+            "fuse_qkv": {
+                "description": "Improve performance at the cost of increased memory usage.",
+                "label": "Fuse QKV projections",
+                "type": "boolean",
+                "default": False,
+            },
+            "compile": {
+                "description": "Use Torch to compile the model for improved performance. Works only on supported platforms.",
+                "label": "Compile",
+                "type": "boolean",
+                "default": False,
+                "onChange": {
+                    True: ['compile_mode', 'compile_fullgraph'],
+                    False: []
+                }
+            },
+            "compile_mode": {
+                "label": "Mode",
+                "type": "string",
+                "default": "max-autotune",
+                "options": ['default', 'reduce-overhead', 'max-autotune', 'max-autotune-no-cudagraphs'],
+            },
+            "compile_fullgraph": {
+                "label": "Full Graph",
+                "type": "boolean",
+                "default": True,
+            },
+            "transformer": { "label": "Transformer", "display": "output", "type": "FluxTransformer2DModel" },
+        }
+    },
+
+    'FluxTextEncoderLoader': {
+        'label': "FLUX Text Encoders Loader",
+        'category': "loader",
+        'params': {
+            "model_id": {
+                "label": "Model",
+                "display": "modelselect",
+                "type": "string",
+                "default": { 'source': 'hub', 'value': 'black-forest-labs/FLUX.1-dev' },
+                "fieldOptions": {
+                    "noValidation": True,
+                    "sources": ['hub'],
+                    "filter": {
+                        "hub": { "className": r"^Flux" },
+                    },
+                },
+            },
+            "dtype": {
+                "label": "Dtype",
+                "type": "string",
+                "default": "bfloat16",
+                "options": ['auto', 'float32', 'float16', 'bfloat16'],
+            },
+            **QUANT_SELECT,
+            **deepcopy(QUANT_FIELDS),
+            "t5": {
+                "label": "T5 Encoder",
+                "display": "input",
+                "type": "T5EncoderModel",
+            },
+            "encoders": {
+                "label": "Encoders",
+                "display": "output",
+                "type": "FluxTextEncoders",
+            },
+        }
+    }
+}
+
+MODULE_MAP['SD3TextEncodersLoader']['params']['quant_exclude']['options'] = T5_LAYERS
+MODULE_MAP['SD3TextEncodersLoader']['params']['quant_exclude']['default'] = T5_LAYERS[-1]
+MODULE_MAP['SD3TransformerLoader']['params']['quant_exclude']['options'] = SD3_LAYERS
+MODULE_MAP['SD3TransformerLoader']['params']['quant_exclude']['default'] = SD3_LAYERS[-1]
+
+MODULE_MAP['FluxTransformerLoader']['params']['quant_exclude']['options'] = FLUX_LAYERS
+MODULE_MAP['FluxTextEncoderLoader']['params']['quant_exclude']['options'] = T5_LAYERS
+MODULE_MAP['FluxTextEncoderLoader']['params']['quant_exclude']['default'] = T5_LAYERS[-1]
+MODULE_MAP['FluxTransformerLoader']['params']['quant_exclude']['default'] = FLUX_LAYERS[-1]
