6fad0fc0491a8b690ba20be2604a1d2f3d4d536c
yiyixuxu <yixu310@gmail.com>
2026-01-29 11:42:43 +0100

    add simple logic to not reload each time click run

--- modules/ModularDiffusers/dynamic_node.py
+++ modules/ModularDiffusers/dynamic_node.py
@@ -6,9 +6,11 @@ from diffusers.modular_pipelines.mellon_node_utils import MellonPipelineConfig
 from mellon.NodeBase import NodeBase
 
 from . import components
+from utils.torch_utils import str_to_dtype, DEFAULT_DEVICE, DEVICE_LIST
 
 
 logger = logging.getLogger("mellon")
+from . import components
 
 
 class DynamicBlockNode(NodeBase):
@@ -37,12 +39,20 @@ class DynamicBlockNode(NodeBase):
             "value": False,
             "onChange": "update_node",
         },
+        "device": {"label": "Device", "type": "string", "value": DEFAULT_DEVICE, "options": DEVICE_LIST},
+        "auto_offload": {"label": "Enable Auto Offload", "type": "boolean", "value": True},
         "doc": {
             "label": "Doc",
             "type": "string",
             "display": "output",
         },
     }
+
+    def __del__(self):
+        node_comp_ids = components._lookup_ids(collection=self.node_id)
+        for comp_id in node_comp_ids:
+            components.remove_from_collection(comp_id, self.node_id)
+        super().__del__()
 
     def _get_custom_params(self, repo_id):
         custom_mellon_config = MellonPipelineConfig.load(repo_id)
@@ -62,11 +72,29 @@ class DynamicBlockNode(NodeBase):
 
     def execute(self, **kwargs):
         repo_id = kwargs.pop("repo_id", "")
-        pipeline = ModularPipeline.from_pretrained(repo_id, trust_remote_code=True, components_manager=components)
+        device = kwargs.pop("device", DEFAULT_DEVICE)
+        auto_offload = kwargs.pop("auto_offload", True)
         
-        # Load config to get input/output names
+        pipeline = ModularPipeline.from_pretrained(
+            repo_id, 
+            trust_remote_code=True, 
+            components_manager=components, 
+            collection=self.node_id
+        )
+
+        # Load config to get input/output names and dtype
         custom_mellon_config = MellonPipelineConfig.load(repo_id)
         node_config = custom_mellon_config.node_params["custom"]
+        
+        # Get dtype from config
+        default_dtype = custom_mellon_config.default_dtype
+        if not default_dtype:
+            default_dtype = "bfloat16"
+        torch_dtype = str_to_dtype(default_dtype)
+        
+        # Enable auto offload if requested
+        if auto_offload and (not components._auto_offload_enabled or components._auto_offload_device != device):
+            components.enable_auto_cpu_offload(device=device)
         
         # Handle components
         components_update_dict = {}
@@ -74,8 +102,29 @@ class DynamicBlockNode(NodeBase):
             if comp_name in kwargs:
                 model_info_dict = kwargs.pop(comp_name)
                 components_update_dict.update(components.get_components_by_ids([model_info_dict["model_id"]]))
+
+
+        # Check which components need to be loaded vs reused
+        components_to_load = []
+        for comp_name in pipeline.pretrained_component_names:
+            if comp_name in components_update_dict:
+                continue  # Already provided externally
+                
+            comp_spec = pipeline.get_component_spec(comp_name)
+            comp_with_same_load_id = components._lookup_ids(load_id=comp_spec.load_id)
+            if comp_with_same_load_id:
+                # Reuse existing component
+                comp_id = list(comp_with_same_load_id)[0]
+                components_update_dict[comp_name] = components.get_one(component_id=comp_id)
+            else:
+                components_to_load.append(comp_name)
+
         pipeline.update_components(**components_update_dict)
-        pipeline.load_components()
+        pipeline.load_components(names=components_to_load, torch_dtype=torch_dtype)
+        
+        # Move to device if not using auto offload
+        if not auto_offload:
+            pipeline.to(device)
         
         # Build inputs dict
         inputs_dict = {}
